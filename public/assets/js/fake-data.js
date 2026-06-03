06d3cad0307e56e0da13bdfbe750e350489c4c50
divinefeliciteyamba-cpu <divinefeliciteyamba@gmail.com>
2026-06-03 18:38:46 +0200

    feat: marqueurs colorés par catégorie avec icônes

--- public/assets/js/fake-data.js
+++ public/assets/js/fake-data.js
@@ -2,7 +2,7 @@
 const FAKE_PMES = [
     {id: 1, nom: "Chez Mère Double",     categorie: "Restauration", lat: -11.632240, lng: 27.460640},
     {id: 2, nom: "Pharmacie Imani",      categorie: "Pharmacie",    lat: -11.664700, lng: 27.419400},
-    {id: 3, nom: "Épicerie du Coin",     categorie: "Epicerie",     lat: -11.668000, lng: 27.485040},
+    {id: 3, nom: "Épicerie du Coin",     categorie: "Epicerie",     lat: -11.648000, lng: 27.485040},
     {id: 4, nom: "TechShop Lushi",       categorie: "Electronique", lat: -11.610000, lng: 27.46000},
     {id: 5, nom: "Restaurant Karibu",    categorie: "Restauration", lat: -11.655000, lng: 27.441000},
 ];
