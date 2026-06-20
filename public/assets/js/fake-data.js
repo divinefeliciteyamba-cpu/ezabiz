b12546543d0cb98ea9098e119678c011e96f4877
divinefeliciteyamba-cpu <divinefeliciteyamba@gmail.com>
2026-06-20 22:56:26 +0200

    feat: page fiche.html avec infos PME et produits dans fake-data

--- public/assets/js/fake-data.js
+++ public/assets/js/fake-data.js
@@ -1,9 +1,67 @@
 // Création des fausses pmes
 const FAKE_PMES = [
-    {id: 1, nom: "Chez Mère Double",     categorie: "Restauration", lat: -11.632240, lng: 27.460640},
-    {id: 2, nom: "Pharmacie Imani",      categorie: "Pharmacie",    lat: -11.664700, lng: 27.419400},
-    {id: 3, nom: "Épicerie du Coin",     categorie: "Epicerie",     lat: -11.648000, lng: 27.485040},
-    {id: 4, nom: "TechShop Lushi",       categorie: "Electronique", lat: -11.610000, lng: 27.46000},
-    {id: 5, nom: "Restaurant Karibu",    categorie: "Restauration", lat: -11.655000, lng: 27.441000},
+    {
+        id: 1, 
+        nom: "Chez Mère Double",     
+        categorie: "Restauration", 
+        lat: -11.632240,
+        lng: 27.460640,
+        telephone: "+243 812 245 678", 
+        horaires: "Lun-Sam : 8h00-20h00", 
+        photo: "assets/images/chez-mere-double.jpg",
+        slogan : "La vraie cuisine congolaise au coeur de Lubumbashi",
+        produits: [
+            {id: 1, nom: "Poulet à la moambe", prix: 8000, photo: "moambe.jpg"},
+            {id: 2, nom: "Riz gras", prix: 5000, photo: "riz.jpg"},
+            {id: 3, nom: "Jus de bissap", prix: 1500, photo: "bissap.jpg"}
+        ]
+    },
+
+    {
+        id: 2, nom: "Pharmacie Imani",     
+        categorie: "Pharmacie",    
+        lat: -11.664700, 
+        lng: 27.419400,
+        telephone: "+243 999 464 936", 
+        horaires: "Lun-Dim : 24h/24, 7j/7", 
+        photo: "assets/images/pharmacie.jpg",
+        slogan : "Votre santé est un investissement, pas une dépense."
+    },
+
+    {
+        id: 3, 
+        nom: "Épicerie du Coin",     
+        categorie: "Epicerie",     
+        lat: -11.648000, 
+        lng: 27.485040,
+        telephone: "+243 902 100 078", 
+        horaires: "Lun-Ven : 8h00-18h00 , Sam : 11h00-17h30", 
+        photo: "assets/images/epicerie-du-coin.jpg",
+        slogan : " Frais, Locaux et de saison."
+    },
+
+    {
+        id: 4, 
+        nom: "TechShop Lushi",      
+        categorie: "Electronique", 
+        lat: -11.610000, 
+        lng: 27.46000,
+        telephone: "+243 827 405 000", 
+        horaires: "Lun-Sam : 8h00-19h30", 
+        photo: "assets/images/technoshop-lushi .jpg",
+        slogan : "Restez connecté !"
+    },
+
+    {
+        id: 5, 
+        nom: "Restaurant Karibu",    
+        categorie: "Restauration", 
+        lat: -11.655000, 
+        lng: 27.441000,
+        telephone: "+243 843 745 568 , +243 995 120 457", 
+        horaires: "Lun-Ven : 8h00-20h30, Sam : 14h30-21h00", 
+        photo: "assets/images/restaurant-karibu.jpg",
+        slogan : "Manger est une nécessité, savourer c'est de l'art"
+    },
 ];
     
